0571b0b2469a548bbae042d18fd6ad632827448e
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-15 16:28:13 -0500

    added main and makefile

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <ncurses.h>
+#include <iostream>
+#include <complex>
+
+int isInSet(std::complex<double> c ) {
+    int explode = 0;
+    std::complex<double> z = c;
+    std::complex<double> z1 = c;
+    for (int i = 0; i < 50; i++) {
+        z = z1;
+        z1 = z*z + c;
+        if(abs(z1) > 2){
+            explode = 1;
+        }
+    }
+    return explode;
+}
+
+
+int main() {
+    std::complex<double> c(-0.3, 0.3);
+    int x = isInSet(c);
+    if (x == 0){
+        std::cout << "In Mandelbrot Set" << x << " ";
+    } else if (x == 1){
+        std::cout << "NOT In Mandelbrot Set"  << x << " ";
+    } else {
+        std::cout << "ERROR" << x << " ";
+    }
+}
