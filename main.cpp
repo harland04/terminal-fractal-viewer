0d11a2467b44a5f92e686bb830648d31088cbc24
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-16 16:53:22 -0500

    did a lot, really messy but im still figuring ncurses out

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <complex>
 
-int isInSet(std::complex<double> c ) {
+bool isInSet(std::complex<double> c ) {
     int explode = 0;
     std::complex<double> z = c;
     std::complex<double> z1 = c;
@@ -10,11 +10,32 @@ int isInSet(std::complex<double> c ) {
         z = z1;
         z1 = z*z + c;
         if(abs(z1) > 2){
-            explode = 1;
+            return false;
         }
     }
-    return explode;
+    return true;
 }
+
+void drawFractal(WINDOW *win, int yMax, int xMax, double yOrigin, double xOrigin, double range, char character) {
+    werase(win);
+    double real = 0;
+    double imag = 0;
+    std::complex<double> complex(0, 0);
+
+    for(int i = 0; i < xMax; i++){
+        for(int j = 0; j < yMax; j++){
+            real = (xOrigin - range/2) + i*(range/xMax);
+            imag = (yOrigin - range/2) + j*(range/yMax);
+            complex.real(real);
+            complex.imag(imag);
+            if(isInSet(complex)){
+                mvwprintw(win, j, i, &character);
+            }
+        }
+    }
+    wrefresh(win);
+}
+
 
 
 int main() {
@@ -30,20 +51,140 @@ int main() {
     //     std::cout << "ERROR" << x << " ";
     // }
 
+    // NCURSES START
     initscr();
     noecho();
     cbreak();
+
+    // Initialize Variables
+    double range = 3;
+    double xOrigin = -0.5;
+    double yOrigin = 0;
+    double zoomFactor = 0.5;
+    char character = '#';
+
+    // Get Terminal Size
     int yMax, xMax;
-
     getmaxyx(stdscr, yMax, xMax);
-    printw("%d %d", yMax, xMax);
-    refresh();
 
-    getch();
-    getmaxyx(stdscr, yMax, xMax);
-    printw("%d %d", yMax, xMax);
+    WINDOW *win = newwin(yMax, xMax, 0,0);
+    box(win, 0, 0);
+    keypad(stdscr, true);
+    keypad(win, true);
     refresh();
+    
+    drawFractal(win, yMax, xMax, yOrigin, xOrigin, range, character);
+    int input;
+    int x = xMax/2;
+    int y = yMax/2;
+    std::string s;
+    move(y, x);
+    refresh();
+    
+    do{
+        input = getch();
+        //     if(input == 10){
+        //         getyx(win, y, x);
+        //         yOrigin = yOrigin - range + y * (range/yMax);
+        //         xOrigin = xOrigin - range + x * (range/xMax);
+        //         range = range*zoomFactor;
+        //         werase(win);
+        //         drawFractal(win, yMax, xMax, yOrigin, xOrigin, range, character);
+        //         mvwprintw(win, 0, 0, "We are zooming");
+        //     }
+        // } 
+        switch(input){
+            case 119:
+                if(y > 0){
+                    s = std::to_string(mvinch(y,x));
+                    wattron(win, A_NORMAL);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_NORMAL);
 
-    getch();
+                    y--;
+                    s = std::to_string(mvinch(y,x));
+
+                    wattron(win, A_REVERSE);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_REVERSE);
+                }
+                refresh();
+                break;
+            case 115:
+                if(y < yMax){
+                    s = std::to_string(mvinch(y,x));
+                    wattron(win, A_NORMAL);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_NORMAL);
+
+                    y++;
+                    s = std::to_string(mvinch(y,x));
+
+                    wattron(win, A_REVERSE);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_REVERSE);
+                }
+                refresh();
+                break;
+            case 100:
+                if(x < xMax){
+                    s = std::to_string(mvinch(y,x));
+                    wattron(win, A_NORMAL);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_NORMAL);
+
+                    x++;
+                    s = std::to_string(mvinch(y,x));
+
+                    wattron(win, A_REVERSE);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_REVERSE);
+                }
+                refresh();
+                break;
+            case 97:
+                if(x > 0){
+                    s = std::to_string(mvinch(y,x));
+                    wattron(win, A_NORMAL);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_NORMAL);
+
+                    x--;
+                    s = std::to_string(mvinch(y,x));
+
+                    wattron(win, A_REVERSE);
+                    mvwprintw(win, y, x, s.c_str());
+                    wattroff(win, A_REVERSE);
+                }
+                refresh();
+                break;
+            default:
+                break;
+        }
+
+
+        if(input == 98){
+            getyx(win, y, x);
+            yOrigin = yOrigin - range + y * (range/yMax);
+            xOrigin = xOrigin - range + x * (range/xMax);
+            range = range*zoomFactor;
+            drawFractal(win, yMax, xMax, yOrigin, xOrigin, range, character);
+            refresh();
+        } else if(input == 118){
+            getyx(win, y, x);
+            yOrigin = yOrigin - range + y * (range/yMax);
+            xOrigin = xOrigin - range + x * (range/xMax);
+            range = range/zoomFactor;
+            drawFractal(win, yMax, xMax, yOrigin, xOrigin, range, character);
+            mvwprintw(win, 0, 0, "We are zooming out");
+            refresh();
+        }        
+
+        mvwprintw(win, 1, 1, "X: %d\rY: %d", x, y);
+        refresh();
+        
+    } while(input != 'x');
+
+
     endwin();
 }
