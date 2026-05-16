4e42c494f4d987cc6fd027ea96c5234c60a1bf67
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-16 11:21:51 -0500

    messed around with some ncurses stuff

--- main.cpp
+++ main.cpp
@@ -18,13 +18,32 @@ int isInSet(std::complex<double> c ) {
 
 
 int main() {
-    std::complex<double> c(-0.3, 0.3);
-    int x = isInSet(c);
-    if (x == 0){
-        std::cout << "In Mandelbrot Set" << x << " ";
-    } else if (x == 1){
-        std::cout << "NOT In Mandelbrot Set"  << x << " ";
-    } else {
-        std::cout << "ERROR" << x << " ";
-    }
+    // double real = 0;
+    // double imag = 0;
+    // std::complex<double> c(-0.3, 0.3);
+    // int x = isInSet(c);
+    // if (x == 0){
+    //     std::cout << "In Mandelbrot Set" << x << " ";
+    // } else if (x == 1){
+    //     std::cout << "NOT In Mandelbrot Set"  << x << " ";
+    // } else {
+    //     std::cout << "ERROR" << x << " ";
+    // }
+
+    initscr();
+    noecho();
+    cbreak();
+    int yMax, xMax;
+
+    getmaxyx(stdscr, yMax, xMax);
+    printw("%d %d", yMax, xMax);
+    refresh();
+
+    getch();
+    getmaxyx(stdscr, yMax, xMax);
+    printw("%d %d", yMax, xMax);
+    refresh();
+
+    getch();
+    endwin();
 }
